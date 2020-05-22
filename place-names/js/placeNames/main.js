d490fd8695cd064b8ab14445912e7687d0e9b9e9
Parker Hodge <iparkerhodge@gmail.com>
2020-05-22 14:44:52 -0500

    completed places-names

--- place-names/js/placeNames/main.js
+++ place-names/js/placeNames/main.js
@@ -1,6 +1,16 @@
 function main() {
   // Put your code here
+  const names = ["Nashville", "Hong Kong", "The back yard", "Earth",
+    "London", "The mall", "Ryman Auditorium", "The Great Wall of China"]
 
+  console.log('All Place Names')
+  names.forEach(name => console.log(name))
+
+  console.log('')
+
+  const theNames = names.filter(name => name.startsWith('The'))
+  console.log("'The' Place Names")
+  theNames.forEach(name => console.log(name))
 }
 
 main();
