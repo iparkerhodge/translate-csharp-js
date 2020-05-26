784aa404cca87eb87c4e00357c452fdb120ef032
Parker Hodge <iparkerhodge@gmail.com>
2020-05-26 13:22:45 -0500

    completed wish-list

--- rolling-dice/csharp/RollingDice/Program.cs
+++ rolling-dice/csharp/RollingDice/Program.cs
@@ -1,33 +1,27 @@
 ﻿using System;
 
-namespace RollingDice
-{
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Let's roll some dice, baby!");
-            Console.WriteLine("---------------------------");
+namespace RollingDice {
+    class Program {
+        static void Main (string[] args) {
+            Console.WriteLine ("Let's roll some dice, baby!");
+            Console.WriteLine ("---------------------------");
 
-            for (int i = 0; i < 10; i++)
-            {
-                Die die1 = Roll();
-                Die die2 = Roll();
+            for (int i = 0; i < 10; i++) {
+                Die die1 = Roll ();
+                Die die2 = Roll ();
 
                 string message = $"{die1} + {die2} == {die1.Value + die2.Value}";
-                if (die1.Value == die2.Value)
-                {
+                if (die1.Value == die2.Value) {
                     message += " DOUBLES!";
                 }
 
-                Console.WriteLine(message);
+                Console.WriteLine (message);
             }
         }
 
-        static Die Roll()
-        {
-            int dieValue = new Random().Next(1, 6);
-            Die die = new Die(dieValue);
+        static Die Roll () {
+            int dieValue = new Random ().Next (1, 6);
+            Die die = new Die (dieValue);
             return die;
         }
     }
@@ -35,27 +29,23 @@ namespace RollingDice
     // This class is used to define a "die" object (a single "dice")
     //  Each die will have a value. This value is the number that the
     //  die "landed on" when it was rolled.
-    public class Die
-    {
+    public class Die {
         public int Value { get; set; }
 
-        public Die(int value)
-        {
+        public Die (int value) {
             this.Value = value;
         }
 
         // This method is automatically called when we create the "message" variable above
         // By the way, a JavaScript object can have a toString() method too...
-        public override string ToString()
-        {
+        public override string ToString () {
             string dieString = "Unknown";
 
             // This "switch" statement behaves like a series of "if/else if" statements.
             //  We compare the numeric Value of the current object accross different "cases".
             //  When we find a matching case we return a string that represents an image of a die
             // Did you kow that JavaScript also has "switch" statements?
-            switch (this.Value)
-            {
+            switch (this.Value) {
                 /* "case 1:" is like the following "if" statement
                 if (this.Value == 1) {
                     dieString = "\u2680";
